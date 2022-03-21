227cee732665b69f411f4ba2db40f1255a538040
Sejal Anand <sejalanand23@gmail.com>
2022-03-21 19:38:54 +0530

    Base NavBar added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,31 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Flashcard Application</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link to="/" class = "nav-link">Home</router-link>
+            </li>
+          </ul>
+          <router-link to="/login" class = "nav-link nav-item">Login</router-link>
+          <router-link class="btn btn-secondary" to ="/register">Register</router-link> 
+        </div>
+      </div>
     </nav>
+    <br/>
     <router-view />
   </div>
 </template>
