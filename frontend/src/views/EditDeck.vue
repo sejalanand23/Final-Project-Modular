86d8663b54f6b52ff8dcd9d5ccb35180f1f08eaa
Sejal Anand <sejalanand23@gmail.com>
2022-03-24 20:16:41 +0530

    Edit Deck and its cards component

---
+++ frontend/src/views/EditDeck.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <h1>Edit Deck</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "EditDeck",
+    data(){
+        return {
+            deck_name : ''
+        }
+    },
+    created(){
+        this.deck_name = this.$route.params.deck_name
+        console.log(this.deck_name)
+    }
+}
+</script>
+
+<style>
+
+</style>
